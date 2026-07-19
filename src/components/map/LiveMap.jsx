b5dbb74bebf05232bbd2fe1e3aaa8bf48a8dc8ae
Rahul Upadhyay <rahulupadhyay6663@gmail.com>
2026-07-19 14:43:14 +0530

    feat: add interactive map with markers

--- src/components/map/LiveMap.jsx
+++ src/components/map/LiveMap.jsx
@@ -1,13 +1,21 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+
+import { officer, suspect, towers } from "../../data/location";
 
 export default function LiveMap() {
   return (
     <div className="bg-white rounded-xl shadow border overflow-hidden">
       <MapContainer
-        center={[32.7266, 74.8570]}
+        center={officer.position}
         zoom={15}
         style={{ height: "500px", width: "100%" }}
       >
@@ -15,6 +23,20 @@ export default function LiveMap() {
           attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        <Marker position={officer.position}>
+          <Popup>Ground Officer</Popup>
+        </Marker>
+
+        <Marker position={suspect.position}>
+          <Popup>Estimated Suspect Location</Popup>
+        </Marker>
+
+        {towers.map((tower) => (
+          <Marker key={tower.id} position={tower.position}>
+            <Popup>{tower.id}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
--- src/data/location.js
+++ src/data/location.js
@@ -1,22 +1,22 @@
 export const officer = {
-  lat: 32.7266,
-  lng: 74.8570,
+  id: "OFF-001",
+  name: "Officer Rahul",
+  position: [32.7266, 74.8570],
 };
 
 export const suspect = {
-  lat: 32.7285,
-  lng: 74.8591,
+  id: "SUS-001",
+  status: "Moving",
+  position: [32.7285, 74.8591],
 };
 
 export const towers = [
   {
-    id: 1,
-    lat: 32.727,
-    lng: 74.858,
+    id: "T001",
+    position: [32.7274, 74.8582],
   },
   {
-    id: 2,
-    lat: 32.725,
-    lng: 74.856,
+    id: "T002",
+    position: [32.7258, 74.8557],
   },
 ];
