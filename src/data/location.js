cfee835eb03757bb0a8b5d28c9aab3188909fb9c
Rahul Upadhyay <rahulupadhyay6663@gmail.com>
2026-07-19 14:29:40 +0530

    feat: add live map foundation

---
+++ src/data/location.js
@@ -0,0 +1,22 @@
+export const officer = {
+  lat: 32.7266,
+  lng: 74.8570,
+};
+
+export const suspect = {
+  lat: 32.7285,
+  lng: 74.8591,
+};
+
+export const towers = [
+  {
+    id: 1,
+    lat: 32.727,
+    lng: 74.858,
+  },
+  {
+    id: 2,
+    lat: 32.725,
+    lng: 74.856,
+  },
+];
